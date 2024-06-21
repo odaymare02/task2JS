d5a59333812c4cce1e38551f6f7129adb86a6cb6
odaymare02 <mareoday123@gmail.com>
2024-06-22 01:26:46 +0300

    final edit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var data="<ul>";
-data+=`<li> C++ </li>`;
-data+=`<li> Java Script</li>`;
-data+=`<li> Python </li>`;
-data+="</ul>";
+var data=`<h2> this is my favorite 3 programming languages</h2>`;
+data+="<ul>";
+var list=[`C++`,`Java Sceipt`,`Python`];
+for(var i=0;i<3;i++)
+    {
+        data+=`<li>${list[i]}</li>`;
+    }
 document.querySelector("div").innerHTML=data;
 document.querySelector("div").classList.add("center");
